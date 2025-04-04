1ea2cb9a5befe961f39ddd7d681cbebe5fd963f1
vp3591 <vipalp1412@outlook.com>
2025-04-04 13:22:48 -0400

    Update main.js
    added the collission effect

--- assignment 4/part 3/main.js
+++ assignment 4/part 3/main.js
@@ -62,3 +62,18 @@ class Ball {
     this.y += this.velY;
   }
   
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }  
+}
+
