2d13b51cdbfc250d5af41b40cb9fdd2ee7e99814
vp3591 <vipalp1412@outlook.com>
2025-04-04 13:11:22 -0400

    Update main.js
    added the formula of the temperature to get the temperature in the celcious and the farenehet.

--- assignment 4/part 1/main.js
+++ assignment 4/part 1/main.js
@@ -36,4 +36,13 @@ function result() {
     newStory = newStory.replaceAll('Bob', name);
   }
 
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
 }
