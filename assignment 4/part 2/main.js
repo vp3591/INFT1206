e804cdf06e8befd0078c3fcdae3f3ef2859f5180
vp3591 <vipalp1412@outlook.com>
2025-04-04 13:17:08 -0400

    Update main.js
    created the image selection in the webpage and the selected image will be poped up anmd get biger

--- assignment 4/part 2/main.js
+++ assignment 4/part 2/main.js
@@ -19,3 +19,16 @@ const alts = {
   'pic5.jpg' : 'Large moth on a leaf'
 }
 
+/* Looping through images */
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
